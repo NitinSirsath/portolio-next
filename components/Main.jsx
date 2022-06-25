0319c2009f2a40db88e29f14a851e50735d4bd76
lostNIII3 <73011138+NitinSirsath@users.noreply.github.com>
2022-06-26 00:45:53 +0530

    bug fixes

--- components/Main.jsx
+++ components/Main.jsx
@@ -15,7 +15,7 @@ const Main = () => {
           </h1>
           <h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
           <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
-            I’m focused on building front-end web applications and learning new
+            I&apos;m focused on building front-end web applications and learning new
             technologies
           </p>
           <div className="flex items-center gap-3 justify-between max-w-[330px] m-auto py-4">
@@ -32,31 +32,7 @@ const Main = () => {
                 </div>
               </a>
             ))}
-            {/* <a
-              href='https://github.com/NitinSirsath'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <FaGithub />
-              </div>
-            </a>
-            <Link href='/#contact'>
-              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <AiOutlineMail />
-              </div>
-            </Link>
-            <Link href='/resume'>
-              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <BsFillPersonLinesFill />
-              </div>
-            </Link>
-            <a  target='_blank'
-              rel='noreferrer'  href='https://www.youtube.com/channel/UCT10UNN_quJOaPPp8uf9VcA'>
-              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <AiFillYoutube />
-              </div>
-            </a> */}
+            
           </div>
         </div>
       </div>
