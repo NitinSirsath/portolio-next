de35aa4c7a8f015de0fffed94b88dcb6448918e7
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2024-06-30 23:54:57 +0530

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -11,7 +11,7 @@ const Main = () => {
             LET&#39;S BUILD SOMETHING TOGETHER
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi, I&#39;m <span className="text-[#5651e5]"> Onkar</span>
+            Hi, I&#39;m <span className="text-[#5651e5]"> Nitin</span>
           </h1>
           <h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
           <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
